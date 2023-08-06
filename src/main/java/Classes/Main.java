b5c063b1d299d04250739f2f5c62f5e243e4cb6f
[Santheep Ganeswaran] <[gshantheep@gmail.com]>
2023-08-06 10:12:35 +0530

    Initial commit

---
+++ src/main/java/Classes/Main.java
@@ -0,0 +1,14 @@
+package Classes;
+
+public class Main {
+    /**
+     *
+     * @param args
+     *
+     * main class which will be running on program launch
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Cruise ship boarding system");
+    }
+}
